3c43e143f7510301edf2853d60e7c8d6b9ca467d
chasegarsee <chasegarsee@gmail.com>
2019-10-15 15:01:39 -0500

    adding some styling libraries

--- FrontEnd/your-tour/src/Components/Restaurants/Restaurants.js
+++ FrontEnd/your-tour/src/Components/Restaurants/Restaurants.js
@@ -5,8 +5,11 @@ export default class Restaurants extends Component {
     render() {
         console.log("PROPS", this.props.item)
         return (
-            < div >
+            <div>
                 <h1>{this.props.item.name}</h1>
+                <p>{this.props.item.genre}</p>
+                <p>{this.props.item.description}</p>
+                <p>{this.props.item.address}</p>
             </div >
         )
     }
