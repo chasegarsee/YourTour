1f97f7d155ad656b49e5fb5cd50d38f58d120784
chasegarsee <chasegarsee@gmail.com>
2019-10-16 18:15:04 -0500

    more tab functionality. still buggy

--- FrontEnd/your-tour/src/Components/Packages/PackageTab.js
+++ FrontEnd/your-tour/src/Components/Packages/PackageTab.js
@@ -2,13 +2,26 @@ import React from "react"
 import { NavLink } from "react-router-dom";
 import OneDayPackage from "./OneDayPackage";
 import TwoDayPackage from "./TwoDayPackage";
+import { Button } from "react-bootstrap"
 
 function PackageTab(props) {
     return (
-        <div style={{ height: "100px" }}>
-            <NavLink to="/one-day-package">One Day Package</NavLink>
-            <NavLink to="/two-day-package">Two Day Package</NavLink>
-            <NavLink to="/weekend-package">Weekend Package</NavLink>
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            justifyContent: "space-between",
+            borderRadius: "7px",
+            padding: "5px"
+        }}>
+            <Button variant="secondary" style={{ borderRadius: "0" }} >
+                <NavLink style={{ color: "white" }} to="/one-day-package">One Day Package</NavLink>
+            </Button>
+            <Button variant="secondary" style={{ borderRadius: "0" }}>
+                <NavLink style={{ color: "white" }} to="/two-day-package">Two Day Package</NavLink>
+            </Button>
+            <Button variant="secondary" style={{ borderRadius: "0" }}>
+                <NavLink style={{ color: "white" }} to="/weekend-package">Weekend Package</NavLink>
+            </Button>
         </div>
     )
 }
