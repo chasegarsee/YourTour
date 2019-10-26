e7322dcdc77a8d17fe97129ce1072db39a9b5d0c
chasegarsee <chasegarsee@gmail.com>
2019-10-25 19:14:17 -0700

    more Auth0. added some private routes. thinking everyone is done except for some settings on the Auth0 site itself. website wont work on this plane lol

--- FrontEnd/your-tour/src/App.js
+++ FrontEnd/your-tour/src/App.js
@@ -1,23 +1,52 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useAuth0 } from "./Components/Auth0/Auth0";
 import "./App.css";
 import Packages from "./Components/Packages/Packages";
 import Places from "./Components/Places/Places";
 import NavBar from "./Components/NavBar";
 import styled from "styled-components";
+import HomePage from "./Components/HomePage";
+import { StyledH1 } from "./styles/Elements";
 
 function App(props) {
+  const { loading } = useAuth0();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <NavBar />
-      <PackageContainer>
-        <Packages id="board-1" className="board" />
-        <Places id="board-2" className="board" />
-      </PackageContainer>
+      <Router>
+        <NavBar />
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <StyledH1>Please Log In</StyledH1>
+        </div>
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/home" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <NavBar />
+<PackageContainer>
+<Packages id="board-1" className="board" />
+<Places id="board-2" className="board" />
+</PackageContainer> */
+}
 
 const PackageContainer = styled.div`
   display: flex;
