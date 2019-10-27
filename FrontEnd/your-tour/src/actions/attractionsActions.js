138a24aacca1e9655fe6cc2fff8d185a0eb8acd7
chasegarsee <chasegarsee@gmail.com>
2019-10-27 13:06:06 +0700

    actions changes

--- FrontEnd/your-tour/src/actions/attractionsActions.js
+++ FrontEnd/your-tour/src/actions/attractionsActions.js
@@ -1,24 +1,24 @@
 import axios from "axios";
-import { BASE_URL } from "../config"
-
+import { BASE_URL } from "../config";
 
 export const GET_ATTRACTIONS_START = "GET_ATTRACTIONS_START";
 export const GET_ATTRACTIONS_SUCCESS = "GET_ATTRACTIONS_SUCCESS";
 export const GET_ATTRACTIONS_FAILURE = "GET_ATTRACTIONS_FAILURE";
 
 export const getAttractions = () => dispatch => {
-    dispatch({ type: GET_ATTRACTIONS_START });
-    return axios.get(`${BASE_URL}/attractions/`, {
-        headers: {
-            "Content-Type": "application.json",
-        }
+  dispatch({ type: GET_ATTRACTIONS_START });
+  return axios
+    .get(`${BASE_URL}/attractions/`, {
+      headers: {
+        "Content-Type": "application.json"
+      }
     })
-        .then(res => {
-            dispatch({ type: GET_ATTRACTIONS_SUCCESS, payload: res.data });
-            console.log(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-            dispatch({ type: GET_ATTRACTIONS_FAILURE, payload: err });
-        });
-}
+    .then(res => {
+      dispatch({ type: GET_ATTRACTIONS_SUCCESS, payload: res.data });
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: GET_ATTRACTIONS_FAILURE, payload: err });
+    });
+};
