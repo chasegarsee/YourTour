279fd471b0bbb6d36919cc80f4b737d90530af78
chasegarsee <chasegarsee@gmail.com>
2019-11-10 14:09:26 -0600

    fixed merge conflicts

--- FrontEnd/your-tour/src/actions/index.js
+++ FrontEnd/your-tour/src/actions/index.js
@@ -1,24 +1,24 @@
 export {
-    GET_RESTAURANTS_START,
-    GET_RESTAURANTS_SUCCESS,
-    GET_RESTAURANTS_FAILURE,
-    getRestaurants
-} from "./restaurantsActions"
+  GET_RESTAURANTS_START,
+  GET_RESTAURANTS_SUCCESS,
+  GET_RESTAURANTS_FAILURE,
+  getRestaurants
+} from "./restaurantsActions";
 
 export {
-    GET_BARS_START,
-    GET_BARS_SUCCESS,
-    GET_BARS_FAILURE,
-    getBars
-} from "./barsActions"
+  GET_BARS_START,
+  GET_BARS_SUCCESS,
+  GET_BARS_FAILURE,
+  getBars
+} from "./barsActions";
 
 export {
-    GET_ATTRACTIONS_START,
-    GET_ATTRACTIONS_SUCCESS,
-    GET_ATTRACTIONS_FAILURE,
-    getAttractions,
-    ADD_ATTRACTION_START,
-    ADD_ATTRACTION_SUCCESS,
-    ADD_ATTRACTION_FAILURE,
-    addAttraction
-} from "./attractionsActions"
+  GET_ATTRACTIONS_START,
+  GET_ATTRACTIONS_SUCCESS,
+  GET_ATTRACTIONS_FAILURE,
+  getAttractions
+} from //ADD_ATTRACTION_START,
+//ADD_ATTRACTION_SUCCESS,
+//ADD_ATTRACTION_FAILURE,
+//addAttraction
+"./attractionsActions";
--- FrontEnd/your-tour/src/reducers/attractionsReducer.js
+++ FrontEnd/your-tour/src/reducers/attractionsReducer.js
@@ -1,72 +1,71 @@
 import {
-    GET_ATTRACTIONS_START,
-    GET_ATTRACTIONS_SUCCESS,
-    GET_ATTRACTIONS_FAILURE,
-    ADD_ATTRACTION_START,
-    ADD_ATTRACTION_SUCCESS,
-    ADD_ATTRACTION_FAILURE
-} from "../actions"
+  GET_ATTRACTIONS_START,
+  GET_ATTRACTIONS_SUCCESS,
+  GET_ATTRACTIONS_FAILURE
+  //ADD_ATTRACTION_START,
+  //ADD_ATTRACTION_SUCCESS,
+  //ADD_ATTRACTION_FAILURE
+} from "../actions";
 
 const initialState = {
-    attractionData: [],
-    getAttractionsStart: false,
-    getAttractionsSuccess: false,
-    getAttractionsFailure: false,
-    addingAttraction: false,
-    addSuccess: false,
-    addFailure: false,
-    error: false,
-}
+  attractionData: [],
+  getAttractionsStart: false,
+  getAttractionsSuccess: false,
+  getAttractionsFailure: false,
+  addingAttraction: false,
+  addSuccess: false,
+  addFailure: false,
+  error: false
+};
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case GET_ATTRACTIONS_START:
-            return {
-                ...state,
-                error: "",
-                getAttractionsStart: true,
-                getAttractionsSuccess: false,
-                getAttractionsFailure: false
-            }
-        case GET_ATTRACTIONS_SUCCESS:
-            return {
-                ...state,
-                getAttractionsStart: false,
-                getAttractionsSuccess: true,
-                getAttractionsFailure: false,
-                attractionData: action.payload
-            }
-        case GET_ATTRACTIONS_FAILURE:
-            return {
-                ...state,
-                getAttractionsStart: false,
-                getAttractionsSuccess: false,
-                getAttractionsFailure: true,
-                error: action.payload
-            }
-        case ADD_ATTRACTION_START:
-            return {
-                ...state,
-                error: false,
-                addingAttraction: true
-            };
-        case ADD_ATTRACTION_SUCCESS:
-            return {
-                ...state,
-                error: false,
-                addingAttraction: false,
-                addSuccess: true,
-                attractionData: action.payload
-            };
-        case ADD_ATTRACTION_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                addingAttraction: false,
-                addFailure: true
-            };
-        default:
-            return state
-    }
-
-}
+  switch (action.type) {
+    case GET_ATTRACTIONS_START:
+      return {
+        ...state,
+        error: "",
+        getAttractionsStart: true,
+        getAttractionsSuccess: false,
+        getAttractionsFailure: false
+      };
+    case GET_ATTRACTIONS_SUCCESS:
+      return {
+        ...state,
+        getAttractionsStart: false,
+        getAttractionsSuccess: true,
+        getAttractionsFailure: false,
+        attractionData: action.payload
+      };
+    case GET_ATTRACTIONS_FAILURE:
+      return {
+        ...state,
+        getAttractionsStart: false,
+        getAttractionsSuccess: false,
+        getAttractionsFailure: true,
+        error: action.payload
+      };
+    // case ADD_ATTRACTION_START:
+    //   return {
+    //     ...state,
+    //     error: false,
+    //     addingAttraction: true
+    //   };
+    // case ADD_ATTRACTION_SUCCESS:
+    //   return {
+    //     ...state,
+    //     error: false,
+    //     addingAttraction: false,
+    //     addSuccess: true,
+    //     attractionData: action.payload
+    //   };
+    // case ADD_ATTRACTION_FAILURE:
+    //   return {
+    //     ...state,
+    //     error: action.payload,
+    //     addingAttraction: false,
+    //     addFailure: true
+    //   };
+    default:
+      return state;
+  }
+};
