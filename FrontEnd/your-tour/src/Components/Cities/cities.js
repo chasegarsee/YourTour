580ce9463a4937df7d54e840ec02b382abaa8170
chasegarsee <chasegarsee@gmail.com>
2019-11-28 12:00:00 -0600

    Printing out some data from the DB

--- FrontEnd/your-tour/src/Components/Cities/cities.js
+++ FrontEnd/your-tour/src/Components/Cities/cities.js
@@ -33,9 +33,60 @@ function Cities(props) {
   return (
     <div>
       <h1>Cities</h1>
-      <p>Some Data Here</p>
-      <p>Some Data Here</p>
-      <p>Some Data Here</p>
+      {data.map(c => (
+        <div key={c.citiesId}>
+          <StyledCard2>
+            <StyledH2>City: {c.City}</StyledH2>
+          </StyledCard2>
+          <div style={{ display: "flex" }}>
+            <div style={{ border: "solid 3px red" }}>
+              <StyledH2>One Day Package</StyledH2>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Attractions</StyledH2>
+                <StyledH2>{c.OneDayPackage.attractions.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Bars</StyledH2>
+                <StyledH2>{c.OneDayPackage.bars.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Restaurants</StyledH2>
+                <StyledH2>{c.OneDayPackage.restaurants.name}</StyledH2>
+              </div>
+            </div>
+            <div style={{ border: "solid 3px red" }}>
+              <StyledH2>Two Day Package</StyledH2>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Attractions</StyledH2>
+                <StyledH2>{c.TwoDayPackage.attractions.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Bars</StyledH2>
+                <StyledH2>{c.TwoDayPackage.bars.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Restaurants</StyledH2>
+                <StyledH2>{c.TwoDayPackage.restaurants.name}</StyledH2>
+              </div>
+            </div>
+            <div style={{ border: "solid 3px red" }}>
+              <StyledH2>Weekend Package</StyledH2>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Attractions</StyledH2>
+                <StyledH2>{c.WeekendPackage.attractions.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Bars</StyledH2>
+                <StyledH2>{c.WeekendPackage.bars.name}</StyledH2>
+              </div>
+              <div style={{ border: "solid 3px skyblue" }}>
+                <StyledH2>Restaurants</StyledH2>
+                <StyledH2>{c.WeekendPackage.restaurants.name}</StyledH2>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
