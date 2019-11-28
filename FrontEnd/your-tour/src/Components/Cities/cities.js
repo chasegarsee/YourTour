fb70b6cde46adad900f02708d95f4f0144f6cf94
chasegarsee <chasegarsee@gmail.com>
2019-11-28 12:00:00 -0600

    Printing out some data from the DB

--- FrontEnd/your-tour/src/Components/Cities/cities.js
+++ FrontEnd/your-tour/src/Components/Cities/cities.js
@@ -30,6 +30,7 @@ function Cities(props) {
       </StyledLoadingDiv>
     );
   }
+
   return (
     <div>
       <h1>Cities</h1>
@@ -72,33 +73,67 @@ function Cities(props) {
               </div>
             </div>
             <div style={{ border: "solid 3px red" }}>
-              <StyledH2>Two Day Package</StyledH2>
+              <StyledH1>Two Day Package</StyledH1>
               <div>
                 <StyledH1>Attractions</StyledH1>
                 <StyledH2>{c.TwoDayPackage.attractions.name}</StyledH2>
+                <StyledP>
+                  Description: {c.TwoDayPackage.attractions.description}
+                </StyledP>
+                <StyledP>Type: {c.TwoDayPackage.attractions.type}</StyledP>
               </div>
               <div>
                 <StyledH1>Bars</StyledH1>
                 <StyledH2>{c.TwoDayPackage.bars.name}</StyledH2>
+                <StyledP>
+                  Description: {c.TwoDayPackage.bars.description}
+                </StyledP>
+                <a href={c.TwoDayPackage.bars.website}>
+                  Website: {c.TwoDayPackage.bars.website}
+                </a>
               </div>
               <div>
                 <StyledH1>Restaurants</StyledH1>
                 <StyledH2>{c.TwoDayPackage.restaurants.name}</StyledH2>
+                <StyledP>
+                  Description: {c.TwoDayPackage.restaurants.description}
+                </StyledP>
+                <StyledP>Genre: {c.TwoDayPackage.restaurants.genre}</StyledP>
+                <a href={c.TwoDayPackage.restaurants.website}>
+                  Website: {c.TwoDayPackage.restaurants.website}
+                </a>
               </div>
             </div>
             <div style={{ border: "solid 3px red" }}>
-              <StyledH2>Weekend Package</StyledH2>
+              <StyledH1>Weekend Package</StyledH1>
               <div>
                 <StyledH1>Attractions</StyledH1>
                 <StyledH2>{c.WeekendPackage.attractions.name}</StyledH2>
+                <StyledP>
+                  Description: {c.WeekendPackage.attractions.description}
+                </StyledP>
+                <StyledP>Type: {c.WeekendPackage.attractions.type}</StyledP>
               </div>
               <div>
                 <StyledH1>Bars</StyledH1>
                 <StyledH2>{c.WeekendPackage.bars.name}</StyledH2>
+                <StyledP>
+                  Description: {c.WeekendPackage.bars.description}
+                </StyledP>
+                <a href={c.WeekendPackage.bars.website}>
+                  Website: {c.WeekendPackage.bars.website}
+                </a>
               </div>
               <div>
                 <StyledH1>Restaurants</StyledH1>
                 <StyledH2>{c.WeekendPackage.restaurants.name}</StyledH2>
+                <StyledP>
+                  Description: {c.WeekendPackage.restaurants.description}
+                </StyledP>
+                <StyledP>Genre: {c.WeekendPackage.restaurants.genre}</StyledP>
+                <a href={c.WeekendPackage.restaurants.website}>
+                  Website: {c.WeekendPackage.restaurants.website}
+                </a>
               </div>
             </div>
           </div>
