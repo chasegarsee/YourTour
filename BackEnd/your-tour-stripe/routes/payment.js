ad5dc02fea3ca5e048feacad3945e9cddf44ff53
chasegarsee <chasegarsee@gmail.com>
2019-12-01 19:28:02 -0600

    updating repo

--- BackEnd/your-tour-stripe/routes/payment.js
+++ BackEnd/your-tour-stripe/routes/payment.js
@@ -1,5 +1,7 @@
 const stripe = require("../constants/stripe");
 
+// const secondStripe = new stripeLoader(process.env.STRIPE_SECRET_KEY);
+
 const postStripeCharge = res => (stripeErr, stripeRes) => {
   if (stripeErr) {
     res.status(500).send({ error: stripeErr });
@@ -7,6 +9,15 @@ const postStripeCharge = res => (stripeErr, stripeRes) => {
     res.status(200).send({ success: stripeRes });
   }
 };
+
+// const charge = (token, amt) => {
+//   return secondStripe.charges.create({
+//     amount: amt * 100,
+//     currency: "usd",
+//     source: token,
+//     description: "Statement Description"
+//   });
+// };
 
 const paymentApi = app => {
   app.get("/", (req, res) => {
@@ -19,6 +30,17 @@ const paymentApi = app => {
   app.post("/", (req, res) => {
     stripe.charges.create(req.body, postStripeCharge(res));
   });
+
+  // app.post("/api/day-package", (req, res) => {
+  //   try {
+  //     let data = charge(req.body.token, req.body.amount);
+  //     console.log("DATTAAA", data);
+  //     res.send("Charged");
+  //   } catch (e) {
+  //     console.log(e);
+  //     res.status(500);
+  //   }
+  // });
 
   return app;
 };
