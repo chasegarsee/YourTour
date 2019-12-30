396062d479befda906152f6cf37dc4c6bca5f250
chasegarsee <chasegarsee@gmail.com>
2019-12-30 13:31:31 -0600

    stripe styles

--- FrontEnd/your-tour/src/STRIPE/StripeForm.js
+++ FrontEnd/your-tour/src/STRIPE/StripeForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import { StyledH1, StyledH2 } from "../styles/Text";
+import { CityButton } from "../styles/Buttons";
 // import { thisTypeAnnotation } from "@babel/types";
 
 class StripeForm extends React.Component {
@@ -36,26 +38,52 @@ class StripeForm extends React.Component {
     this.props.history.push(`/${lastURLSegment}`);
   };
 
+  createOptions = () => {
+    return {
+      style: {
+        base: {
+          color: "white",
+          backgroundColor: "#1f303b",
+          width: "100%",
+          fontSize: "25px",
+          fontWeight: "100",
+          letterSpacing: "0.025em",
+          "::placeholder": {
+            color: "white"
+          }
+        },
+        invalid: {
+          color: "red"
+        }
+      }
+    };
+  };
+
   render() {
     return (
       <div>
-        <h1>Stripe Checkout</h1>
-        <form onSubmit={this.handleSubmit}>
-          <h2>Name</h2>
+        <StyledH1>Stripe Checkout</StyledH1>
+        <form
+          onSubmit={this.handleSubmit}
+          style={{ width: "1000px", height: "1000px" }}
+        >
+          <StyledH2>Name</StyledH2>
           <input
             type="text"
             value={this.state.name}
             onChange={e => this.setState({ name: e.target.value })}
           />
-          <h2>Amount</h2>
+          <StyledH2>Amount</StyledH2>
           <input
             type="text"
             value={this.state.amount}
             onChange={e => this.setState({ amount: e.target.value })}
           />
-          <h2>Card Information</h2>
-          <CardElement />
-          <button>Charge me!</button>
+          <StyledH2>Card Information</StyledH2>
+
+          <CardElement {...this.createOptions()} />
+
+          <CityButton>Pay</CityButton>
         </form>
       </div>
     );
